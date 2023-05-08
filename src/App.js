18f9f0a84896fd1c325566b70970a0b328c3e7db
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2023-05-08 13:41:50 +0100

    displays weather data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
 const api = {
   key: '9d54b4134840423050e9a3f21b40dc15',
-  base: 'https://api.openweathermap.org'
+  base: 'https://api.openweathermap.org/data/2.5/'
 
-}
-
-
-const searchPressed = () => {
-  console.log("serach pressen")
 }
 
 function App() {
   const[search,setSearch] = useState("");
+  const[weather,setWeather] = useState("");
+
+
+  const searchPressed = () => {
+    console.log("Searching for:" + search);
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        setWeather(result)
+      });
+  }
 
   return (
     <div className="App">
@@ -32,11 +38,21 @@ function App() {
           <button onClick={searchPressed} >Search</button>
          </div>
 
-        {/* Location*/}
+        {typeof weather.main !== "undefined" ? (
+          <div>
+            <p>Location: {weather.name}, {weather.sys.country}</p>
+            <p>Temp: {weather.main.temp}°C</p>
+            <p>Humidity: {weather.main.humidity}%</p>
+            <p>Windspeed: {weather.wind.speed}</p>
+            <p>Wind direction: {weather.wind.deg} degrees</p>
 
-        {/* Temperature F/C*/}
-
-        {/* */}
+          </div>
+        ) : (
+          <div>
+            <p>Please search for a City above</p>
+          </div>
+        )}
+  
 
       </header>
     </div>
