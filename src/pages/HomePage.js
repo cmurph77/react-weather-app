d2cec737e45325bc00c13b7abf8f4620c62dba73
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2023-05-09 16:03:31 +0100

    created home page and navbar

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+
+export default function HomePage() {
+  return (
+
+    <div className='homePage'>
+        <Navbar></Navbar>
+        <h1>Welcome to the HomePage</h1>
+    </div>
+  )
+}
